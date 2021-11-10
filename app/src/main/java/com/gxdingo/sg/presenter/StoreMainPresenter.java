93edc69b0cd94dd776fdbb3af20340453b1a6dbd
PC-20210316HTMY\Administrator <649030309@qq.com>
2021-11-10 14:02:10 +0800

    合并

--- app/src/main/java/com/gxdingo/sg/presenter/StoreMainPresenter.java
+++ app/src/main/java/com/gxdingo/sg/presenter/StoreMainPresenter.java
@@ -36,6 +36,7 @@ public class StoreMainPresenter extends BaseMvpPresenter<BasicsListener, StoreMa
         networkModel = new NetworkModel(this);
     }
 
+    
     @Override
     public void persenterInit() {
 
