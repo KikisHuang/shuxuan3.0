098a0a6e671ce9a8cca09c105a43e81b4c84d4a4
PC-20210316HTMY\Administrator <649030309@qq.com>
2021-11-10 14:12:01 +0800

    阿里receiver

---
+++ app/src/main/java/com/gxdingo/sg/receiver/AliPushMessageReceiver.java
@@ -0,0 +1,132 @@
+package com.gxdingo.sg.receiver;
+
+import android.content.Context;
+
+import com.alibaba.sdk.android.push.MessageReceiver;
+import com.alibaba.sdk.android.push.noonesdk.PushServiceFactory;
+import com.alibaba.sdk.android.push.notification.CPushMessage;
+import com.blankj.utilcode.util.GsonUtils;
+import com.blankj.utilcode.util.LogUtils;
+import com.blankj.utilcode.util.SPUtils;
+import com.gxdingo.sg.utils.LocalConstant;
+
+import org.greenrobot.eventbus.EventBus;
+
+import java.util.Map;
+
+import static com.blankj.utilcode.util.StringUtils.isEmpty;
+import static com.gxdingo.sg.utils.LocalConstant.LOGIN_WAY;
+import static com.kikis.commnlibrary.utils.IntentUtils.getIntentEntityMap;
+import static com.kikis.commnlibrary.utils.IntentUtils.goToPagePutSerializable;
+import static com.kikis.commnlibrary.utils.KikisUitls.getContext;
+
+
+/**
+ * Created by Kikis on 2020/12/16.
+ * 推送receiver
+ */
+public class AliPushMessageReceiver extends MessageReceiver {
+
+    public static final String TAG = "Shuxiang_User_Push";
+
+
+    /**
+     * 当通知准确到达用户的时候触发
+     *
+     * @param context
+     * @param title
+     * @param summary
+     * @param extraMap
+     */
+    @Override
+    public void onNotification(Context context, String title, String summary, Map<String, String> extraMap) {
+
+        LogUtils.d(TAG, "Receive notification, title: " + title + ", summary: " + summary + ", extraMap: " + extraMap);
+
+        String orderId = extraMap.get("orderId");
+
+/*
+        if (!isEmpty(orderId))
+            EventBus.getDefault().post(new OrderRefreshBean(Long.valueOf(orderId)));
+*/
+
+        //应用不是后台状态，不显示通知栏
+        if (!LocalConstant.isBackground)
+            PushServiceFactory.getCloudPushService().clearNotifications();
+    }
+
+
+    /**
+     * 当消息准确到达用户的时候触发，只有用户在使用的时候才能接收到消息
+     *
+     * @param context
+     * @param cPushMessage
+     */
+    @Override
+    public void onMessage(Context context, CPushMessage cPushMessage) {
+        LogUtils.d(TAG, "onMessage, messageId: " + cPushMessage.getMessageId() + ", title: " + cPushMessage.getTitle() + ", content:" + cPushMessage.getContent());
+    }
+
+    /**
+     * 当通知打开的时候触发的操作
+     *
+     * @param context
+     * @param title
+     * @param summary
+     * @param extraMap
+     */
+    @Override
+    public void onNotificationOpened(Context context, String title, String summary, String extraMap) {
+        LogUtils.d(TAG, "onNotificationOpened, title: " + title + ", summary: " + summary + ", extraMap:" + extraMap);
+
+        try {
+
+
+        } catch (Exception e) {
+            LogUtils.e("ali push paser error == " + e);
+        }
+    }
+
+    /**
+     * 当通知被点击了触发的操作，并且没有配置跳转路径
+     *
+     * @param context
+     * @param title
+     * @param summary
+     * @param extraMap
+     */
+    @Override
+    protected void onNotificationClickedWithNoAction(Context context, String title, String summary, String extraMap) {
+        LogUtils.d(TAG, "onNotificationClickedWithNoAction, title: " + title + ", summary: " + summary + ", extraMap:" + extraMap);
+
+
+    }
+
+    /**
+     * 当用户使用时接收到通知
+     *
+     * @param context
+     * @param title
+     * @param summary
+     * @param extraMap
+     * @param openType
+     * @param openActivity
+     * @param openUrl
+     */
+    @Override
+    protected void onNotificationReceivedInApp(Context context, String title, String summary, Map<String, String> extraMap, int openType, String openActivity, String openUrl) {
+        LogUtils.d(TAG, "onNotificationReceivedInApp, title: " + title + ", summary: " + summary + ", extraMap:" + extraMap + ", openType:" + openType + ", openActivity:" + openActivity + ", openUrl:" + openUrl);
+    }
+
+    /**
+     * 通知被移除
+     *
+     * @param context
+     * @param messageId
+     */
+    @Override
+    protected void onNotificationRemoved(Context context, String messageId) {
+        LogUtils.d(TAG, "onNotificationRemoved");
+
+    }
+}
