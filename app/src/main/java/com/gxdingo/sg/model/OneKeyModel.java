c50b74526fa67030b959678a63ef9bcde0dc85c9
Weaving <272803601@qq.com>
2021-11-19 15:27:45 +0800

    一键登陆页面第三方登录

--- app/src/main/java/com/gxdingo/sg/model/OneKeyModel.java
+++ app/src/main/java/com/gxdingo/sg/model/OneKeyModel.java
@@ -1,15 +1,20 @@
 package com.gxdingo.sg.model;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.os.Build;
+import android.os.Handler;
+import android.os.Message;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.blankj.utilcode.util.LogUtils;
@@ -17,18 +22,24 @@ import com.blankj.utilcode.util.SPUtils;
 import com.blankj.utilcode.util.ToastUtils;
 import com.google.gson.reflect.TypeToken;
 import com.gxdingo.sg.R;
+import com.gxdingo.sg.activity.BindingPhoneActivity;
 import com.gxdingo.sg.activity.LoginActivity;
+import com.gxdingo.sg.bean.AuthResult;
 import com.gxdingo.sg.bean.NormalBean;
 import com.gxdingo.sg.bean.OneKeyLoginEvent;
 import com.gxdingo.sg.bean.UserBean;
+import com.gxdingo.sg.bean.WeChatLoginEvent;
 import com.gxdingo.sg.biz.NetWorkListener;
 import com.gxdingo.sg.http.Api;
 import com.gxdingo.sg.http.ClientApi;
 import com.gxdingo.sg.http.HttpClient;
+import com.gxdingo.sg.utils.ClientLocalConstant;
 import com.gxdingo.sg.utils.LocalConstant;
 import com.gxdingo.sg.utils.UserInfoUtils;
+import com.gxdingo.sg.utils.WechatUtils;
 import com.gxdingo.sg.view.MyBaseSubscriber;
 import com.kikis.commnlibrary.biz.CustomResultListener;
+import com.kikis.commnlibrary.utils.Constant;
 import com.mobile.auth.gatewayauth.AuthRegisterXmlConfig;
 import com.mobile.auth.gatewayauth.AuthUIConfig;
 import com.mobile.auth.gatewayauth.AuthUIControlClickListener;
@@ -37,22 +48,27 @@ import com.mobile.auth.gatewayauth.ResultCode;
 import com.mobile.auth.gatewayauth.TokenResultListener;
 import com.mobile.auth.gatewayauth.model.TokenRet;
 import com.mobile.auth.gatewayauth.ui.AbstractPnsViewDelegate;
+import com.tencent.mm.opensdk.modelmsg.SendAuth;
 import com.zhouyou.http.callback.CallClazzProxy;
 import com.zhouyou.http.exception.ApiException;
 import com.zhouyou.http.model.ApiResult;
 
 import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 
 import java.util.Map;
 
 import io.reactivex.Observable;
 
+import static com.blankj.utilcode.util.StringUtils.getString;
 import static com.gxdingo.sg.http.Api.GET_MOBILE_KEY;
 import static com.gxdingo.sg.http.Api.HTTP;
 import static com.gxdingo.sg.http.Api.L;
 import static com.gxdingo.sg.http.Api.ONE_CLICK_LOGIN;
 import static com.gxdingo.sg.http.Api.PAYMENT_ALIPAY_AUTHINFO;
 import static com.gxdingo.sg.http.Api.SM;
+import static com.gxdingo.sg.http.Api.USER_OPEN_LOGIN;
 import static com.gxdingo.sg.http.Api.isUat;
 import static com.gxdingo.sg.http.ClientApi.ARTICLE_DETAIL;
 import static com.gxdingo.sg.http.ClientApi.CLIENT_PORT;
@@ -65,12 +81,19 @@ import static com.gxdingo.sg.http.ClientApi.STORE_SERVICE_AGREEMENT_KEY;
 import static com.gxdingo.sg.http.ClientApi.UAT_URL;
 import static com.gxdingo.sg.http.HttpClient.switchGlobalUrl;
 import static com.gxdingo.sg.utils.LocalConstant.LOGIN_WAY;
+import static com.gxdingo.sg.utils.LocalConstant.SDK_AUTH_FLAG;
 import static com.gxdingo.sg.utils.LocalConstant.STORE_LOGIN_SUCCEED;
+import static com.gxdingo.sg.utils.pay.AlipayTool.auth;
 import static com.kikis.commnlibrary.utils.CommonUtils.getc;
 import static com.kikis.commnlibrary.utils.CommonUtils.getd;
+import static com.kikis.commnlibrary.utils.CommonUtils.gets;
+import static com.kikis.commnlibrary.utils.CommonUtils.isWeixinAvilible;
 import static com.kikis.commnlibrary.utils.Constant.isDebug;
 import static com.kikis.commnlibrary.utils.GsonUtil.getJsonMap;
+import static com.kikis.commnlibrary.utils.IntentUtils.getIntentEntityMap;
 import static com.kikis.commnlibrary.utils.IntentUtils.goToPage;
+import static com.kikis.commnlibrary.utils.IntentUtils.goToPagePutSerializable;
+import static com.kikis.commnlibrary.utils.KikisUitls.getContext;
 import static com.kikis.commnlibrary.utils.StringUtils.isEmpty;
 
 /**
@@ -91,6 +114,7 @@ public class OneKeyModel {
     private String url = isUat ? HTTP + UAT_URL : !isDebug ? HTTP + ClientApi.OFFICIAL_URL : HTTP + ClientApi.TEST_URL + SM + CLIENT_PORT + L;
 
     public OneKeyModel() {
+        EventBus.getDefault().register(this);
         isUser = SPUtils.getInstance().getBoolean(LOGIN_WAY, true);
     }
 
@@ -104,7 +128,7 @@ public class OneKeyModel {
 
         if (!isEmpty(LocalConstant.AUTH_SECRET))
             sdkInit(context, customResultListener);
-         else {
+        else {
 
             Map<String, String> map = getJsonMap();
 
@@ -197,7 +221,7 @@ public class OneKeyModel {
      *
      * @param context
      */
-    public void getAliAuthInfo(Context context,CustomResultListener customResultListener) {
+    public void getAliAuthInfo(Context context) {
         Observable<NormalBean> observable = HttpClient.post(PAYMENT_ALIPAY_AUTHINFO)
                 .execute(new CallClazzProxy<ApiResult<NormalBean>, NormalBean>(new TypeToken<NormalBean>() {
                 }.getType()) {
@@ -208,23 +232,94 @@ public class OneKeyModel {
             public void onError(ApiException e) {
                 super.onError(e);
                 LogUtils.e(e);
-
-//                if (netWorkListener != null) {
-//                    netWorkListener.onAfters();
-//                    netWorkListener.onMessage(e.getMessage());
-//                }
             }
 
             @Override
             public void onNext(NormalBean normalBean) {
 
-                if (customResultListener != null)
-                    customResultListener.onResult(normalBean.auth);
+//                if (customResultListener != null)
+//                    customResultListener.onResult(normalBean.auth);
+                auth((Activity) context, normalBean.auth, handler);
             }
         };
 
         observable.subscribe(subscriber);
     }
+
+    /**
+     * 第三方登录
+     *
+     * @param code
+     * @param type
+     */
+    public void thirdPartyLogin(Context context, String code, String type, boolean isUse) {
+
+        if (TextUtils.isEmpty(code)) {
+            ToastUtils.showLong(gets(R.string.phone_number_can_not_null));
+            return;
+        } else if (TextUtils.isEmpty(type)) {
+            ToastUtils.showLong(gets(R.string.verification_code_can_not_null));
+            return;
+        }
+
+
+        Map<String, String> map = getJsonMap();
+
+        map.put(LocalConstant.APPNAME, type);
+        map.put(Constant.CODE, code);
+
+        Observable<UserBean> observable = HttpClient.post(USER_OPEN_LOGIN, map)
+                .execute(new CallClazzProxy<ApiResult<UserBean>, UserBean>(new TypeToken<UserBean>() {
+                }.getType()) {
+                });
+
+        MyBaseSubscriber subscriber = new MyBaseSubscriber<UserBean>(context) {
+            @Override
+            public void onError(ApiException e) {
+                super.onError(e);
+                LogUtils.e(e);
+            }
+
+            @Override
+            public void onNext(UserBean userBean) {
+                OneKeyModel.quitLoginPage();
+                //0未绑定手机
+                if (userBean.getIsBindMobile() == 0) {
+
+                    ToastUtils.showLong(gets(R.string.please_bind_phone));
+//                    netWorkListener.onSucceed(LocalConstant.BIND_PHONE);
+                    goToPagePutSerializable(context, BindingPhoneActivity.class, getIntentEntityMap(new Object[]{userBean.getOpenid(), type, isUse}));
+                } else {
+                    UserInfoUtils.getInstance().saveLoginUserInfo(userBean);
+                    EventBus.getDefault().post(isUse ? LocalConstant.CLIENT_LOGIN_SUCCEED : STORE_LOGIN_SUCCEED);
+                }
+            }
+        };
+
+        observable.subscribe(subscriber);
+    }
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+//            super.handleMessage(msg);
+            switch (msg.what) {
+                case SDK_AUTH_FLAG:
+                    AuthResult authResult = (AuthResult) msg.obj;
+//                    if (mNetworkModel!=null){
+//                        if (!TextUtils.isEmpty(authResult.getAuthCode())) {
+//                            mNetworkModel.thirdPartyLogin(getContext(), authResult.getAuthCode(), ClientLocalConstant.ALIPAY, getV().isClient());
+//                        } else
+//                            onMessage("没有获取到authCode");
+//                    }
+                    if (!TextUtils.isEmpty(authResult.getAuthCode())) {
+                        thirdPartyLogin(getContext(), authResult.getAuthCode(), ClientLocalConstant.ALIPAY, isUser);
+                    } else
+                        ToastUtils.showLong("没有获取到authCode");
+                    break;
+            }
+        }
+    };
 
     public void sdkInit(Context context, CustomResultListener customResultListener) {
 
@@ -313,18 +408,27 @@ public class OneKeyModel {
                         findViewById(R.id.alipay_login).setOnClickListener(new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
-                                getAliAuthInfo(getContext(), new CustomResultListener() {
-                                    @Override
-                                    public void onResult(Object o) {
-
-                                    }
-                                });
+                                getAliAuthInfo(getContext());
 //                                EventBus.getDefault().post(LocalConstant.ALIPAY_LOGIN_EVENT);
                             }
                         });
                         findViewById(R.id.wechat_login).setOnClickListener(new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
+
+                                if (isWeixinAvilible(getContext()))
+                                {
+
+                                    final SendAuth.Req req = new SendAuth.Req();
+
+                                    req.scope = "snsapi_userinfo";
+                                    req.state = "wechat_sdk_demo";
+
+                                    WechatUtils.getInstance().getWxApi().sendReq(req);
+                                }
+                                else {
+                                    ToastUtils.showLong(String.format(getString(R.string.uninstall_app), gets(R.string.wechat)));
+                                }
 //                                customResultListener.onResult(new OneKeyLoginEvent("",isUser,2));
                             }
                         });
@@ -368,6 +472,16 @@ public class OneKeyModel {
                 .setScreenOrientation(authPageOrientation)
                 .create());
         mAuthHelper.getLoginToken(context, 2000);
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onWechatEvent(Object object){
+        //微信登录事件
+        if (object instanceof WeChatLoginEvent) {
+            WeChatLoginEvent event = (WeChatLoginEvent) object;
+            if (!TextUtils.isEmpty(event.code))
+                thirdPartyLogin(getContext(), event.code, ClientLocalConstant.WECHAT, isUser);
+        }
     }
 
     private void settingButtnStatus(View view) {
