6735dc27b42f998a905b7febf15a1105a5e328dd
Weaving <272803601@qq.com>
2021-12-13 09:00:39 +0800

    合并

--- app/src/main/java/com/gxdingo/sg/activity/WebActivity.java
+++ app/src/main/java/com/gxdingo/sg/activity/WebActivity.java
@@ -13,9 +13,12 @@ import com.blankj.utilcode.util.ToastUtils;
 import com.gxdingo.sg.R;
 import com.gxdingo.sg.bean.UserBean;
 import com.gxdingo.sg.bean.WebBean;
+import com.gxdingo.sg.bean.WheelResultBean;
 import com.gxdingo.sg.biz.WebContract;
 import com.gxdingo.sg.biz.WebViewLoadingListener;
 import com.gxdingo.sg.presenter.WebPresenter;
+import com.gxdingo.sg.utils.LocalConstant;
+import com.gxdingo.sg.utils.ShareUtils;
 import com.gxdingo.sg.utils.UserInfoUtils;
 import com.gxdingo.sg.view.MyWebChromeClient;
 import com.kikis.commnlibrary.activitiy.BaseMvpActivity;
@@ -25,6 +28,8 @@ import com.tencent.smtt.sdk.ValueCallback;
 import com.tencent.smtt.sdk.WebView;
 import com.tencent.smtt.sdk.WebViewClient;
 import com.umeng.socialize.UMShareAPI;
+import com.umeng.socialize.UMShareListener;
+import com.umeng.socialize.bean.SHARE_MEDIA;
 
 import java.util.List;
 
@@ -237,6 +242,62 @@ public class WebActivity extends BaseMvpActivity<WebContract.WebPresenter> imple
 //            IntentUtils.goToPage(reference.get(), ClientComplainActivity.class, null);
     }
 
+    @JavascriptInterface
+    public void backToApp(String restultData) {
+        LogUtils.d("h5调用了"+restultData);
+        WheelResultBean wheelResultBean = GsonUtil.GsonToBean(restultData, WheelResultBean.class);
+        if (wheelResultBean==null)return;
+        if (wheelResultBean.jumpType == 30){
+            ShareUtils.UmShare(this, new UMShareListener() {
+                @Override
+                public void onStart(SHARE_MEDIA share_media) {
+                    LogUtils.d("onStart:"+share_media);
+                }
+
+                @Override
+                public void onResult(SHARE_MEDIA share_media) {
+                    LogUtils.d("onResult:"+share_media);
+                    getP().completeTask();
+                }
+
+                @Override
+                public void onError(SHARE_MEDIA share_media, Throwable throwable) {
+                    LogUtils.d("onError:"+share_media);
+                }
+
+                @Override
+                public void onCancel(SHARE_MEDIA share_media) {
+                    LogUtils.d("onCancel:"+share_media);
+                }
+            },wheelResultBean.url,wheelResultBean.title,wheelResultBean.describe,R.mipmap.ic_app_logo,SHARE_MEDIA.WEIXIN_CIRCLE);
+        }else if (wheelResultBean.jumpType == 20){
+            ShareUtils.UmShare(this, new UMShareListener() {
+                @Override
+                public void onStart(SHARE_MEDIA share_media) {
+
+                }
+
+                @Override
+                public void onResult(SHARE_MEDIA share_media) {
+
+                }
+
+                @Override
+                public void onError(SHARE_MEDIA share_media, Throwable throwable) {
+
+                }
+
+                @Override
+                public void onCancel(SHARE_MEDIA share_media) {
+
+                }
+            },wheelResultBean.url,wheelResultBean.title,wheelResultBean.describe,R.mipmap.ic_app_logo,SHARE_MEDIA.WEIXIN);
+
+        }else if (wheelResultBean.jumpType == 10){
+            sendEvent(LocalConstant.VISIT_CIRCLE);
+            finish();
+        }
+    }
 
     /**
      * 获取token
